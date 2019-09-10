2d74051195294b0d07baf7b2ffae106c23747d07
woles <woles00@gmail.com>
2019-09-10 09:05:37 +0200

    Updates

---
+++ precache-manifest.17e97b177d1822cecf8aca976f7d83ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cac1f8203d10bb9cbd45d85bc27de47",
+    "url": "/woles/react-pie3d-demo.git/index.html"
+  },
+  {
+    "revision": "60d22635a37003c28fd0",
+    "url": "/woles/react-pie3d-demo.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ec78046c57d25140055",
+    "url": "/woles/react-pie3d-demo.git/static/js/2.36327965.chunk.js"
+  },
+  {
+    "revision": "60d22635a37003c28fd0",
+    "url": "/woles/react-pie3d-demo.git/static/js/main.5750e0e0.chunk.js"
+  },
+  {
+    "revision": "2f8e51edffdd1770191d",
+    "url": "/woles/react-pie3d-demo.git/static/js/runtime~main.960128ad.js"
+  }
+]);
